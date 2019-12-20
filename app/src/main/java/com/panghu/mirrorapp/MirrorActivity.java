a10801241ce8b48902523a1394727164948cb7ea
lijiangping <414029565@qq.com>
2019-12-20 18:06:39 +0800

    [init] init app

---
+++ app/src/main/java/com/panghu/mirrorapp/MirrorActivity.java
@@ -0,0 +1,15 @@
+package com.panghu.mirrorapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MirrorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mirror);
+
+
+    }
+}
